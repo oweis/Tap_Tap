3dd6733ca2ad174253f3ecf5fd45d9849f13bf68
aferiat <aichaaferiat@gmail.com>
2016-08-19 20:07:08 +0100

    remove unused imports

--- app/src/main/java/com/example/user/taptap/TapTap.java
+++ app/src/main/java/com/example/user/taptap/TapTap.java
@@ -1,10 +1,7 @@
 package com.example.user.taptap;
 
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
